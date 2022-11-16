6bc52a24408094901338e150fba053a23744ce00
Sainath Chilkuri <chilkurisainath@gmail.com>
2022-11-16 13:00:08 +0530

    package created

---
+++ lib/demo_package.dart
@@ -0,0 +1,20 @@
+library demo_package;
+
+import 'package:flutter/material.dart';
+
+class DemoWidget extends StatefulWidget {
+  const DemoWidget({Key? key}) : super(key: key);
+
+  @override
+  _DemoWidgetState createState() => _DemoWidgetState();
+}
+
+class _DemoWidgetState extends State<DemoWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text("Demo Wsssidget Package"),
+    );
+  }
+}
+
